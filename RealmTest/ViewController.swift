f2e862f67f04a607687705968685c2fab85f642f
yukiny0811 <yukiny0811@gmail.com>
2022-09-26 00:29:24 +0900

    aaa

--- RealmTest/ViewController.swift
+++ RealmTest/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let image = UIImage(named: "test")!
+        
+        let filename = UUID.init().uuidString + ".jpg"
+        image.saveToDocuments(filename: filename)
+        
+        let newImage = UIImage.getFromDocuments(filename: filename)
+        imageView.image = newImage
     }
-
-
 }
 
+extension UIImage {
+    func saveToDocuments(filename:String){
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let fileURL = documentsDirectory.appendingPathComponent(filename)
+        if let data = self.jpegData(compressionQuality: 1.0) {
+            do {
+                try data.write(to: fileURL)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    static func getFromDocuments(filename: String) -> UIImage {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let data = try! Data(contentsOf: documentsDirectory.appendingPathComponent(filename))
+        let image = UIImage(data: data)
+        return image!
+    }
+}
